c5ac145364b29d0be267570844f33708212f2a3a
insertt <inserttdev@gmail.com>
2021-03-17 21:54:57 +0100

    Make it compile again

--- src/aicensor.rs
+++ src/aicensor.rs
@@ -1,11 +1,11 @@
-use crate::OkaeriSdkError;
 use crate::client::OkaeriClient;
+use crate::OkaeriSdkError;
+use serde::Deserialize;
 use serde_json::json;
+use std::collections::HashMap;
 use std::env;
 use std::time::Duration;
 use url::Url;
-use std::collections::HashMap;
-use serde::{Deserialize};
 
 type Result<T> = std::result::Result<T, OkaeriSdkError>;
 
@@ -43,7 +43,7 @@ pub struct CensorPredictionInfo {
 }
 
 pub struct AiCensor {
-    client: OkaeriClient
+    client: OkaeriClient,
 }
 
 impl AiCensor {
@@ -51,23 +51,25 @@ impl AiCensor {
         AiCensor::new_with_config(token, None, None)
     }
 
-    pub fn new_with_config<S: Into<String>>(token: S, base_url: Option<&str>, timeout: Option<Duration>) -> Result<Self> {
+    pub fn new_with_config<S: Into<String>>(
+        token: S,
+        base_url: Option<&str>,
+        timeout: Option<Duration>,
+    ) -> Result<Self> {
         let base_url = OkaeriClient::read_base_url(
             base_url,
             "https://ai-censor.okaeri.eu",
-            "OKAERI_SDK_AICENSOR_BASE_PATH")?;
-        let timeout = OkaeriClient::read_timeout(
-            timeout,
-            Duration::from_secs(5),
-            "OKAERI_SDK_TIMEOUT")?;
-        let headers = OkaeriClient::singleton_map(
-            String::from("Token"), token.into());
+            "OKAERI_SDK_AICENSOR_BASE_PATH",
+        )?;
+        let timeout =
+            OkaeriClient::read_timeout(timeout, Duration::from_secs(5), "OKAERI_SDK_TIMEOUT")?;
+        let headers = maplit::hashmap! { String::from("token") => token.into() };
         let client = OkaeriClient::new(base_url, timeout, headers)?;
         Ok(AiCensor { client })
     }
 
     pub(crate) async fn get_prediction(self, phrase: &str) -> Result<CensorPredictionInfo> {
         let body = json!({"phrase": phrase.to_owned()});
-        self.client.post::<CensorPredictionInfo>("/predict", &*body.to_string()).await
+        self.client.post("/predict", &*body.to_string()).await
     }
 }
--- src/client.rs
+++ src/client.rs
@@ -1,12 +1,13 @@
-use hyper::client::HttpConnector;
-use hyper_tls::HttpsConnector;
-use hyper_timeout::TimeoutConnector;
-use hyper::{Client, Method, Request, Body};
 use crate::OkaeriSdkError;
-use std::time::Duration;
-use url::Url;
+use hyper::client::HttpConnector;
+use hyper::{Body, Client, Method, Request};
+use hyper_timeout::TimeoutConnector;
+use hyper_tls::HttpsConnector;
+use serde::de::DeserializeOwned;
 use std::collections::HashMap;
 use std::env;
+use std::time::Duration;
+use url::Url;
 
 type Result<T> = std::result::Result<T, OkaeriSdkError>;
 
@@ -24,81 +25,100 @@ impl OkaeriClient {
         connector.set_read_timeout(Some(timeout));
         connector.set_write_timeout(Some(timeout));
         let hyper = Client::builder().build::<_, hyper::Body>(connector);
-        Ok(OkaeriClient { base_url, hyper, headers })
+        Ok(OkaeriClient {
+            base_url,
+            hyper,
+            headers,
+        })
     }
 
     pub(crate) fn read_base_url(provided: Option<&str>, def: &str, env_name: &str) -> Result<Url> {
         let base_url = match env::var(env_name) {
             Ok(value) => value,
-            Err(_) => String::from(provided.unwrap_or(def))
+            Err(_) => String::from(provided.unwrap_or(def)),
         };
-        let base_url = Url::parse(base_url.as_str())
-            .map_err(|source| OkaeriSdkError::InvalidUrl { url: base_url, source })?;
+        let base_url =
+            Url::parse(base_url.as_str()).map_err(|source| OkaeriSdkError::InvalidUrl {
+                url: base_url,
+                source,
+            })?;
         Ok(base_url)
     }
 
-    pub(crate) fn read_timeout(provided: Option<Duration>, def: Duration, env_name: &str) -> Result<Duration> {
+    pub(crate) fn read_timeout(
+        provided: Option<Duration>,
+        def: Duration,
+        env_name: &str,
+    ) -> Result<Duration> {
         let timeout = match env::var(env_name) {
             Ok(from) => {
-                let value = from.parse::<u64>().map_err(|_| OkaeriSdkError::InvalidInt { from })?;
+                let value = from
+                    .parse::<u64>()
+                    .map_err(|_| OkaeriSdkError::InvalidInt { from })?;
                 Duration::from_millis(value)
             }
-            Err(_) => provided.unwrap_or(def)
+            Err(_) => provided.unwrap_or(def),
         };
         Ok(timeout)
     }
 
-    pub(crate) fn singleton_map<K, V>(key: K, value: V) -> HashMap<K, V> {
-        let mut headers: HashMap<K, V> = HashMap::new();
-        headers.insert(key, value);
-        return headers
-    }
+    pub(crate) async fn post<T>(self, path: impl AsRef<str>, body: impl Into<String>) -> Result<T>
+    where
+        T: DeserializeOwned,
+    {
+        let path = path.as_ref();
+        let body = body.into();
 
-    pub(crate) async fn post<T>(self, path: &str, body: &str) -> Result<T> {
         let url = format!("{}{}", self.base_url, path);
 
-        let mut req = Request::builder()
-            .method(Method::POST)
-            .uri(url);
+        let mut req = Request::builder().method(Method::POST).uri(url);
 
         for (key, value) in self.headers {
             req = req.header(key.as_str(), value.as_str());
         }
 
-        let req = req.body(Body::from(body))
+        let req = req
+            .body(Body::from(body))
             .map_err(|err| OkaeriSdkError::ResponseError {
                 group: String::from("REQUEST_ERROR"),
-                message: String::from(format!("failed to create request: {}", err)),
+                message: format!("failed to create request: {}", err),
             })?;
 
-        let res = self.hyper.request(req).await
+        let res = self
+            .hyper
+            .request(req)
+            .await
             .map_err(|err| OkaeriSdkError::ResponseError {
                 group: String::from("REQUEST_ERROR"),
-                message: String::from(format!("failed to dispatch request: {}", err)),
+                message: format!("failed to dispatch request: {}", err),
             })?;
 
         if !res.status().is_success() {
             let error = OkaeriSdkError::ResponseError {
                 group: String::from("REQUEST_ERROR"),
-                message: String::from(format!("received invalid status code {}", res.status())),
+                message: format!("received invalid status code {}", res.status()),
             };
             return Err(error);
         }
 
-        let bytes = hyper::body::to_bytes(res).await
-            .map_err(|err| OkaeriSdkError::ResponseError {
+        let bytes =
+            hyper::body::to_bytes(res)
+                .await
+                .map_err(|err| OkaeriSdkError::ResponseError {
+                    group: String::from("REQUEST_ERROR"),
+                    message: format!("failed to process request: {}", err),
+                })?;
+
+        let body_str =
+            String::from_utf8(bytes.to_vec()).map_err(|err| OkaeriSdkError::ResponseError {
                 group: String::from("REQUEST_ERROR"),
-                message: String::from(format!("failed to process request: {}", err)),
+                message: format!("failed to convert body to string: {}", err),
             })?;
 
-        let body_str = String::from_utf8(bytes.to_vec())
-            .map_err(|err| OkaeriSdkError::ResponseError {
-                group: String::from("REQUEST_ERROR"),
-                message: String::from(format!("failed to convert body to string: {}", err)),
+        let info: T =
+            serde_json::from_str(&body_str).map_err(|_| OkaeriSdkError::ResponseParseError {
+                body: body_str.clone(),
             })?;
-
-        let info: T = serde_json::from_reader(&*body_str)
-            .map_err(|_| OkaeriSdkError::ResponseParseError { body: body_str })?;
 
         Ok(info)
     }
