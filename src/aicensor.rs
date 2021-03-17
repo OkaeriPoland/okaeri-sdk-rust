b386aae8a8c2d63e2f97545ed69d0541c10f6ebd
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-03-17 19:11:17 +0100

    Implement timeout, code cleanup

--- src/aicensor.rs
+++ src/aicensor.rs
@@ -1,13 +1,15 @@
 use crate::OkaeriSdkError;
 use hyper::{Body, Client, Method, Request};
-use serde::Deserialize;
+use hyper_tls::HttpsConnector;
 use serde_json::json;
 use std::env;
 use std::time::Duration;
 use url::Url;
+use hyper_timeout::TimeoutConnector;
 
 type Result<T> = std::result::Result<T, OkaeriSdkError>;
 
+#[allow(dead_code)]
 #[derive(Deserialize)]
 pub struct CensorPredictionInfoGeneral {
     pub(crate) swear: bool,
@@ -15,6 +17,7 @@ pub struct CensorPredictionInfoGeneral {
     pub(crate) domains: bool,
 }
 
+#[allow(dead_code)]
 #[derive(Deserialize)]
 pub struct CensorPredictionInfoDetails {
     pub(crate) basic_contains_hit: bool,
@@ -24,12 +27,14 @@ pub struct CensorPredictionInfoDetails {
     pub(crate) domains_list: Vec<String>,
 }
 
+#[allow(dead_code)]
 #[derive(Deserialize)]
 pub struct CensorPredictionInfoElapsed {
     pub(crate) all: f64,
     pub(crate) processing: f64,
 }
 
+#[allow(dead_code)]
 #[derive(Deserialize)]
 pub struct CensorPredictionInfo {
     pub(crate) general: CensorPredictionInfoGeneral,
@@ -48,39 +53,26 @@ impl AiCensor {
         AiCensor::new_with_config(token, None, None)
     }
 
-    pub fn new_with_config<S: Into<String>>(
-        token: S,
-        base_url: Option<&str>,
-        timeout: Option<Duration>,
-    ) -> Result<Self> {
+    pub fn new_with_config<S: Into<String>>(token: S, base_url: Option<&str>, timeout: Option<Duration>) -> Result<Self> {
         let token = token.into();
 
         let base_url = match env::var("OKAERI_SDK_AICENSOR_BASE_PATH") {
             Ok(value) => value,
-            Err(_) => String::from(base_url.unwrap_or("https://ai-censor.okaeri.eu")),
+            Err(_) => String::from(base_url.unwrap_or("https://ai-censor.okaeri.eu"))
         };
 
-        let base_url =
-            Url::parse(base_url.as_str()).map_err(|source| OkaeriSdkError::InvalidUrl {
-                url: base_url,
-                source,
-            })?;
+        let base_url = Url::parse(base_url.as_str())
+            .map_err(|source| OkaeriSdkError::InvalidUrl { url: base_url, source })?;
 
         let timeout = match env::var("OKAERI_SDK_TIMEOUT") {
             Ok(from) => {
-                let value = from
-                    .parse::<u64>()
-                    .map_err(|_| OkaeriSdkError::InvalidInt { from })?;
+                let value = from.parse::<u64>().map_err(|_| OkaeriSdkError::InvalidInt { from })?;
                 Duration::from_millis(value)
             }
-            Err(_) => timeout.unwrap_or(Duration::from_secs(5)),
+            Err(_) => timeout.unwrap_or(Duration::from_secs(5))
         };
 
-        Ok(AiCensor {
-            base_url,
-            timeout,
-            token,
-        })
+        Ok(AiCensor { base_url, timeout, token })
     }
 
     pub(crate) async fn get_prediction(self, phrase: &str) -> Result<CensorPredictionInfo> {
@@ -89,23 +81,27 @@ impl AiCensor {
             "phrase": phrase.to_owned()
         });
 
-        let client = Client::new();
+        let https = HttpsConnector::new();
+        let mut connector = TimeoutConnector::new(https);
+        connector.set_connect_timeout(Some(self.timeout));
+        connector.set_read_timeout(Some(self.timeout));
+        connector.set_write_timeout(Some(self.timeout));
+        let client = Client::builder().build::<_, hyper::Body>(connector);
+
         let req = Request::builder()
             .method(Method::POST)
             .uri(url)
-            .header("Authorization", format!("Bearer {}", self.token))
+            .header("Token", self.token)
             .body(Body::from(body.to_string()))
             .map_err(|err| OkaeriSdkError::ResponseError {
                 group: String::from("REQUEST_ERROR"),
-                message: String::from("failed to create request"),
+                message: String::from(format!("failed to create request: {}", err)),
             })?;
 
-        let res = client
-            .request(req)
-            .await
+        let res = client.request(req).await
             .map_err(|err| OkaeriSdkError::ResponseError {
                 group: String::from("REQUEST_ERROR"),
-                message: String::from("failed to dispatch request"),
+                message: String::from(format!("failed to dispatch request: {}", err)),
             })?;
 
         if !res.status().is_success() {
@@ -116,22 +112,20 @@ impl AiCensor {
             return Err(error);
         }
 
-        let bytes =
-            hyper::body::to_bytes(res)
-                .await
-                .map_err(|err| OkaeriSdkError::ResponseError {
-                    group: String::from("REQUEST_ERROR"),
-                    message: String::from("failed to process request"),
-                })?;
-
-        let body_str =
-            String::from_utf8(bytes.to_vec()).map_err(|err| OkaeriSdkError::ResponseError {
+        let bytes = hyper::body::to_bytes(res).await
+            .map_err(|err| OkaeriSdkError::ResponseError {
                 group: String::from("REQUEST_ERROR"),
-                message: String::from("failed to convert body to string"),
+                message: String::from(format!("failed to process request: {}", err)),
+            })?;
+
+        let body_str = String::from_utf8(bytes.to_vec())
+            .map_err(|err| OkaeriSdkError::ResponseError {
+                group: String::from("REQUEST_ERROR"),
+                message: String::from(format!("failed to convert body to string: {}", err)),
             })?;
 
         let info: CensorPredictionInfo = serde_json::from_str(&*body_str)
-            .map_err(|error| OkaeriSdkError::ResponseParseError { body: body_str })?;
+            .map_err(|_| OkaeriSdkError::ResponseParseError { body: body_str })?;
 
         Ok(info)
     }
--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,8 @@ mod tests {
 
     #[tokio::test]
     async fn aicensor() -> TestResult {
-        let aicensor = AiCensor::new("")?;
+
+        let aicensor = AiCensor::new("94433c4b-6ae2-479e-a051-731a2ef1919a")?;
         let prediction = aicensor.get_prediction("hehe").await?;
         let swear = prediction.general.swear;
         println!("swear: {}", swear);
